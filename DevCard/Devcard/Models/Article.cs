0e7f70340f044046250217a17ea6204780e05cd5
Babak-Samadi <148950679+BabakSamadi@users.noreply.github.com>
2024-10-12 16:41:28 +0330

    partial view index

---
+++ DevCard/Devcard/Models/Article.cs
@@ -0,0 +1,12 @@
+﻿namespace Devcard.Models;
+
+public class Article
+{
+
+	public long id { get; set; }
+
+	public string title { get; set; }
+
+	public string discription { get; set; }
+
+}
---
+++ DevCard/Devcard/Models/project.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Build.Evaluation;
+
+namespace Devcard.Models
+{
+	public class project
+	{
+
+		public long id { get; set; }
+
+		public string name { get; set; }
+
+		public string discription { get; set; }
+
+		public  string client { get; set;  }
+		
+
+	}
+
+
+	//پاس دادن دو تا مدل در قالب یک کلاس که بصورت دو تا پراپرتی لیست هست 
+	public class indexModel
+	{
+		public List<Project> Projects { get; set; }
+		public List<Article> Articles { get; set; }
+	}
+}
